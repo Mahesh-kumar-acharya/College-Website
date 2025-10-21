ad23fb33c452da433077d272d94091c2c56c5d03
Mahesh Kumar Acharya <epicc668@gmail.com>
2025-10-21 21:34:13 +0530

    Add files via upload

---
+++ Imperial college/script.js
@@ -0,0 +1,11 @@
+function instagram(){
+    window.location.href = "https://www.instagram.com/imperial_college_berhampur?igsh=MWpod3NuOHI4d3MwNQ=="
+}
+
+function facebook(){
+    window.location.href = "https://www.facebook.com/share/17qCx6Xv4h/"
+}
+
+function youtube(){
+    window.location.href = "https://youtube.com/@imperialcollege305?si=mDkP-1yDlfPOk89_"
+}
